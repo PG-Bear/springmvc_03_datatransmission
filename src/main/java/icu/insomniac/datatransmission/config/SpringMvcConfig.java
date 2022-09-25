5b2bb5d7c1b240e2a88711200fdc545c3630b3cc
PG-Bear <kiting427914@gmail.com>
2022-09-25 13:41:22 +0800

    update project

--- src/main/java/icu/insomniac/datatransmission/config/SpringMvcConfig.java
+++ src/main/java/icu/insomniac/datatransmission/config/SpringMvcConfig.java
@@ -1,7 +1,9 @@
 package icu.insomniac.datatransmission.config;
 
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @ComponentScan("icu.insomniac.datatransmission.controller")
+@EnableWebMvc
 public class SpringMvcConfig {
 }
